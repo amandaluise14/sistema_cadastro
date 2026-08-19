fab2287664b8ae84a9fd1eec68dc493107f2fb99
amandaluise14 <amanda_l_maia@estudante.sesisenai.org.br>
2026-08-19 09:50:52 -0300

    fix - corrigi um erro na pagina cadastrar pratos.

--- public/pratos/cadastrar_pratos.php
+++ public/pratos/cadastrar_pratos.php
@@ -6,12 +6,13 @@ $nome = $_POST["nome"];
 $descricao = $_POST["descricao"];
 $preco = $_POST["preco"];
 $categoria = $_POST["categoria"];
+$usuario_id = $_POST["usuario_id"];
 
 $stmt = $conexao->prepare(
-    "INSERT INTO prato (nome, descricao, preco, categoria) VALUES (?, ?, ?, ?)"
+    "INSERT INTO prato (nome, descricao, preco, categoria, usuario_id) VALUES (?, ?, ?, ?, ?)"
 );
 
-$stmt -> bind_param ("ssis", $nome, $descricao, $preco, $categoria);
+$stmt -> bind_param ("ssis", $nome, $descricao, $preco, $categoria, $usuario_id);
 $stmt -> execute();
 
 

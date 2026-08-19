2dfc15fb53a756e20560343300b165099fb96a7b
amandaluise14 <amanda_l_maia@estudante.sesisenai.org.br>
2026-08-19 18:51:40 -0300

    add - adicionei a pagina de listar partos.

---
+++ public/pratos/listar_pratos.php
@@ -0,0 +1,59 @@
+<?php
+
+include "infra/conexao.php";
+$usuarios = mysqli_query($conexao, "SELECT * FROM usuario");
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Restaurante Anamandas</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+    <header>
+        <h1>Restaurante Anamandas</h1>
+    </header>
+    <main>
+        <h2>Cadastro de Usuarios</h2>
+        <form action="public/usuario/cadastrar_usuario.php" method="POST">
+            <label for="nome">Nome:</label>
+            <input type="text" name="nome">
+            <br>
+            <label for="email">Email:</label>
+            <input type="text" name="email">
+            <br>
+            <button type="submit">Cadastrar</button>
+        </form>
+        <div>
+            <h2></h2>
+            <table>
+                <tr>
+                    <th>ID</th>
+                    <th>Nome</th>
+                    <th>Email</th>
+                </tr>
+                <?php while ($usuarios = mysqli_fetch_assoc($usuario)) { ?>
+                    <tr>
+                        <td><?php echo $usuario["id"] ?></td>
+                        <td><?php echo $usuario["nome"] ?></td>
+                        <td><?php echo $usuario["email"] ?></td>
+                    </tr>
+                <?php } ?>
+            </table>
+        </div>
+
+    </main>
+    <footer>
+
+    </footer>
+
+
+</body>
+
+</html> 
